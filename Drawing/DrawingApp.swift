ceeec081379a1e62c063885937c0d9bd871b0976
Matthew Dolan <mdolancode@gmail.com>
2023-03-14 09:48:03 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Matthew Dolan on 2023-03-14.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
